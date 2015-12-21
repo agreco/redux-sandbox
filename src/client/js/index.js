2525031557da3e7e9d96ef3871b64759e01fd538
Antonio Greco <agreco13@gmail.com>
2015-12-21 18:57:38 +0000

    Abstract components

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -67,6 +67,18 @@ const FilterLink = ({ filter, currentFilter, children }) => {
     )
 };
 
+const Todo = ({ onClick, completed, text }) => (
+  <li onClick={ onClick } style={{ textDecoration: completed ? 'line-through' : 'none' }}> {text} </li>
+);
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+      {
+        todos.map(todo =>  <Todo key={todo.id} {...todo} onClick={ () => onTodoClick(todo.id) } />)
+      }
+  </ul>
+);
+
 const getVisibleTodos = (todos, filter ) => {
     switch (filter) {
         case 'SHOW_ALL':
@@ -96,17 +108,7 @@ class TodoApp extends Component {
                     });
                     this.input.value = '';
                 }}> Add Todo </button>
-                <ul>
-                    { visibleTodos.map(todo => {
-                        return (
-                            <li key={todo.id}
-                                onClick={() => { store.dispatch({ type: 'TOGGLE_TODO', id: todo.id }); }}
-                                style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-                                {todo.text}
-                            </li>
-                        )
-                    }) }
-                </ul>
+                <TodoList todos={visibleTodos} onTodoClick={ id => store.dispatch({ type: 'TOGGLE_TODO', id }) } />
                 <p>
                     Show:
                     {' '}
